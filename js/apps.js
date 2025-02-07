4ecf568fa398d920b6b6ddefa53689239d3edaf4
Adrian Oliveros <adrianrobbieoliveros@gmail.com>
2025-02-07 16:38:41 -0500

    Update apps.js

--- js/apps.js
+++ js/apps.js
@@ -1,1 +1,12 @@
+// Verificar la edad al entrar a la página de bebidas
+if (window.location.pathname.includes('bebidas.html')) {
+    const añoNacimiento = prompt("Por favor, ingresa tu año de nacimiento:");
+    const añoActual = new Date().getFullYear();
+    const edad = añoActual - añoNacimiento;
 
+    if (edad >= 18) {
+        alert("Puedes escoger también las bebidas con alcohol.");
+    } else {
+        alert("No puedes escoger bebidas con alcohol.");
+    }
+}
